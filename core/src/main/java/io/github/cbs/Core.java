8ae444f4f0fdd7a67ebaa5c78aa4ac5146e54459
Suraj-Mohapatra <suraj2895@gmail.com>
2025-10-08 02:37:36 +0530

    adds core module

--- core/src/main/java/io/github/cbs/Core.java
+++ core/src/main/java/io/github/cbs/Core.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CoreBankingSystemApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CoreBankingSystemApplication.class, args);
-	}
-
+public class Core {
+    public static void main(String[] args) {
+        SpringApplication.run(Core.class, args);
+        System.out.println("Core started");
+    }
 }
