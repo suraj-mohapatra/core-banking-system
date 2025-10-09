a5fe723b1cb5d82d65029a5fc1623d9d628ccd64
Suraj-Mohapatra <suraj2895@gmail.com>
2025-10-10 03:13:49 +0530

    setup

--- core/src/main/java/io/github/cbs/web/WebController.java
+++ core/src/main/java/io/github/cbs/web/WebController.java
@@ -1,8 +1,15 @@
 package io.github.cbs.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class WebController {
+    @GetMapping({"/", ""})
+    public String foo() {
+        return "Hello from CBS!";
+    }
 
 }
