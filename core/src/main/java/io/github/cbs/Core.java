0f24ca2063d51d82241531bf88c71b107dee23a9
Suraj-Mohapatra <suraj2895@gmail.com>
2025-10-16 03:39:51 +0530

    adds entities

--- core/src/main/java/io/github/cbs/Core.java
+++ core/src/main/java/io/github/cbs/Core.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Core {
     public static void main(String[] args) {
         SpringApplication.run(Core.class, args);
-        System.out.println("Core started..."); // s
+        System.out.println("Core started...");
     }
 }
