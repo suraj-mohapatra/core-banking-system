f3004e728d9d646250cb76d7af8b367f94637ee5
Suraj-Mohapatra <suraj2895@gmail.com>
2026-01-20 03:18:26 +0530

    rafactor

--- api/src/main/java/io/github/cbs/web/WebController.java
+++ api/src/main/java/io/github/cbs/web/WebController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @Tag(name = "Web Controller", description = "Basic API for CBS")
 @RestController
 @RequestMapping("/api")
